7fa28fcf14e6d1fd15bee0cd90d5ae87740a4c8b
Aman Devchandani <74649624+amandevchandani@users.noreply.github.com>
2022-03-16 18:51:28 +0530

    Add files via upload

---
+++ RetailIntelligence/webdatacollectors/meijer_fruits_web.py
@@ -0,0 +1,60 @@
+import json
+import requests
+from extract import json_extract
+#from addict import Dict
+from datetime import date
+
+
+for i in range(0, 6):
+    def fetch_data():
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0',
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Origin': 'https://www.meijer.com',
+            'Connection': 'keep-alive',
+            'Referer': 'https://www.meijer.com/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'TE': 'trailers',
+        }
+
+        params = (
+            ('c', 'ciojs-client-2.15.0'),
+            ('key', 'key_GdYuTcnduTUtsZd6'),
+            ('i', '2799e664-7630-4157-a8a7-1c66b8722d79'),
+            ('s', '2'),
+            ('us', 'web'),
+            ('page', i+1),
+            ('filters[availableInStores]', '20'),
+            ('sort_by', 'relevance'),
+            ('sort_order', 'descending'),
+            ('fmt_options[groups_max_depth]', '3'),
+            ('fmt_options[groups_start]', 'current'),
+            ('_dt', '1645543509718'),
+        )
+
+        response = requests.get('https://ac.cnstrc.com/browse/group_id/L3-3197', headers=headers, params=params)
+        data_fetched = response.json()
+        return data_fetched
+
+
+    def store_data():
+
+        today = str(date.today())
+        json_dump = json.dumps(fetch_data())
+        with open("data/meijer/meijer_fruits/" + today + ".json", "a") as outfile:
+            outfile.write(json_dump)
+
+
+    store_data()
+
+date = str(date.today())
+print('### ' + date + '- All Data Stored ###')
+
+
+
+
---
+++ RetailIntelligence/webdatacollectors/walmart_fruits_web.py
@@ -0,0 +1,67 @@
+from addict import Dict
+from extract import json_extract
+import requests
+import json
+from walmart_curl_details import *
+from datetime import date
+
+
+def fetch_data_first_products():
+
+    response_first_products = requests.post('https://www.walmart.ca/api/bsp/carousel/spr', headers=headers, params=params_first_products, cookies=cookies_first_products, json=json_data_first_products)
+
+    fetch_first_products_json = response_first_products.json()
+
+    # print(fetch_first_products_json)
+
+    return fetch_first_products_json
+
+
+def fetch_data_product():
+
+    response_fetch_product = requests.post('https://www.walmart.ca/api/bsp/fetch-products', headers=headers, cookies=cookies_fetch_products, json=json_data_fetch_products)
+
+    fetch_product_json = response_fetch_product.json()
+
+    # print(fetch_product_json)
+
+    return fetch_product_json
+
+
+def fetch_data_price():
+
+    response_fetch_price = requests.post('https://www.walmart.ca/api/bsp/v2/price-offer', headers=headers, cookies=cookies_fetch_price, json=json_data_fetch_price)
+
+    fetch_price_json = response_fetch_price.json()
+
+    # print(fetch_price_json)
+
+    return fetch_price_json
+
+
+def store_data():
+
+    today = str(date.today())
+
+    json_dump_first_products = json.dumps(fetch_data_first_products())
+    with open("data/walmart/walmart_fruits/"+today+".json", "w") as outfile:
+        outfile.write(json_dump_first_products)
+
+    json_dump_products = json.dumps(fetch_data_product())
+    with open("data/walmart/walmart_fruits/"+today+".json", "a") as outfile:
+        outfile.write(json_dump_products)
+
+    json_dump_price = json.dumps(fetch_data_price())
+    with open("data/walmart/walmart_fruits/"+today+".json", "a") as outfile:
+        outfile.write(json_dump_price)
+
+    print('### '+today+'- All Data Stored ###')
+
+
+store_data()
+
+'''
+data = Dict(response.json())
+price = json_extract(data, 'currentPrice')
+print(price)
+'''
